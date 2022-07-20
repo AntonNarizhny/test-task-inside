dade7ba93a01b056fab7971511d16868866141c8
Anton Narizhny <anton.narizhny@gmail.com>
2022-07-20 20:06:26 +0300

    Initial Commit

---
+++ src/main/java/ru/narizhny/test_task_inside/TestTaskInsideApplication.java
@@ -0,0 +1,13 @@
+package ru.narizhny.test_task_inside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskInsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskInsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/narizhny/test_task_inside/TestTaskInsideApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.narizhny.test_task_inside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskInsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
